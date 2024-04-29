09c77797dfe6dc68303fbbc0d8822a973fa05c26
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2024-04-29 15:23:41 -0500

    cambiando diseño

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
+import { CiPlay1, CiTrash } from "react-icons/ci";
+import { IoIosAdd } from "react-icons/io";
 
 function App() {
   const [processes, setProcesses] = useState([]);
@@ -85,107 +87,127 @@ function App() {
   };
 
   return (
-    <div>
-      <h2>Simulador Round Robin</h2>
-      <label>
-        Quantum:
-        <input
-          type="number"
-          disabled={isRunning === true}
-          value={quantum}
-          onChange={handleQuantumChange}
-        />
-      </label>
-      <br />
-      <h3>Introducir Procesos</h3>
-      <form>
-        {processes.map((process, index) => (
-          <div key={index}>
-            <label>
-              Proceso {index + 1}{" "}
-              <div>
-                Tiempo de Ráfaga de CPU:
-                <input
-                  disabled={isRunning === true}
-                  type="number"
-                  value={process.burstTime || ""}
-                  onChange={(e) => handleProcessChange(e, index)}
-                />
-              </div>
-              <div>
-                Prioridad:
-                <input
-                  disabled={isRunning === true}
-                  type="number"
-                  value={process.burstTime || ""}
-                  onChange={(e) => handleProcessChange(e, index)}
-                />
-              </div>
-              <div>
-                Entradas/Salidas:
-                <input
-                  disabled={isRunning === true}
-                  type="number"
-                  value={process.burstTime || ""}
-                  onChange={(e) => handleProcessChange(e, index)}
-                />
-              </div>
-            </label>
+    <div className="main">
+      <h2 className="header">Simulador Round Robin</h2>
+      <div className="mainContainer">
+        <div className="actions">
+          <div className="input">
+            <label>Quantum</label>
+            <input
+              type="number"
+              disabled={isRunning === true}
+              value={quantum}
+              onChange={handleQuantumChange}
+            />
           </div>
-        ))}
-      </form>
-      <br />
-      <button onClick={addProcess} disabled={isRunning === true}>
-        Agregar Proceso
-      </button>
-      <button onClick={runRoundRobin} disabled={isRunning === true}>
-        Ejecutar Round Robin
-      </button>
-      <button onClick={borrar}>Borrar</button>
-      <br />
-      <h3>Registro de Ejecución</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Proceso</th>
-            <th>Tiempo de Inicio</th>
-            <th>Tiempo de Finalización</th>
-            <th>Tiempo de Espera</th>
-            <th>Tiempo de Retorno</th>
-          </tr>
-        </thead>
-        <tbody>
-          {executionLog.map((log, index) => (
-            <tr key={index}>
-              <td>{log.process}</td>
-              <td>{log.startTime}</td>
-              <td>{log.endTime}</td>
-              <td>{log.waitingTime}</td>
-              <td>{log.turnaroundTime}</td>
-            </tr>
-          ))}
-          <tr>
-            <td colSpan="3"></td>
-            <td>Tiempo de Espera Promedio:</td>
-            <td>
-              {executionLog.length > 0 &&
-                executionLog.reduce((acc, curr) => acc + curr.waitingTime, 0) /
-                  executionLog.length}
-            </td>
-          </tr>
-          <tr>
-            <td colSpan="3"></td>
-            <td>Tiempo de Retorno Promedio:</td>
-            <td>
-              {executionLog.length > 0 &&
-                executionLog.reduce(
-                  (acc, curr) => acc + curr.turnaroundTime,
-                  0
-                ) / executionLog.length}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+          <div className="buttons">
+            <button
+              className="round"
+              onClick={runRoundRobin}
+              disabled={isRunning === true}
+            >
+              <CiPlay1 /> <span>Ejecutar Round Robin</span>
+            </button>
+            <button
+              className="add"
+              onClick={addProcess}
+              disabled={isRunning === true}
+            >
+              <IoIosAdd /> <span>Agregar Proceso</span>
+            </button>
+            <button className="delete" onClick={borrar}>
+              <CiTrash />
+              <span>Borrar</span>
+            </button>
+          </div>
+        </div>
+        {processes.length > 0 && (
+          <div className="form">
+            <>
+              <>
+                <h3 className="headerForm">Introducir Procesos</h3>
+              </>
+              <>
+                <form>
+                  {processes.map((process, index) => (
+                    <div className="process" key={index}>
+                      <p>Proceso {index + 1} </p>
+
+                      <div className="input">
+                        <label>Tiempo de Ráfaga de CPU</label>
+                        <input
+                          disabled={isRunning === true}
+                          type="number"
+                          value={process.burstTime || ""}
+                          onChange={(e) => handleProcessChange(e, index)}
+                        />
+                      </div>
+                      <div className="input">
+                        <label>Entradas/Salidas</label>
+                        <input
+                          disabled={isRunning === true}
+                          type="number"
+                          value={process.burstTime || ""}
+                          onChange={(e) => handleProcessChange(e, index)}
+                        />
+                      </div>
+                    </div>
+                  ))}
+                </form>
+              </>
+            </>
+          </div>
+        )}
+        <div>
+          <h3>Registro de Ejecución</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Proceso</th>
+                <th>Tiempo de Inicio</th>
+                <th>Tiempo de Finalización</th>
+                <th>Tiempo de Espera</th>
+                <th>Tiempo de Retorno</th>
+              </tr>
+            </thead>
+            <tbody>
+              {executionLog.map((log, index) => (
+                <tr key={index}>
+                  <td>{log.process}</td>
+                  <td>{log.startTime}</td>
+                  <td>{log.endTime}</td>
+                  <td>{log.waitingTime}</td>
+                  <td>{log.turnaroundTime}</td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan="3"></td>
+                <td>Tiempo de Espera Promedio:</td>
+                <td>
+                  {executionLog.length > 0 &&
+                    executionLog.reduce(
+                      (acc, curr) => acc + curr.waitingTime,
+                      0
+                    ) / executionLog.length}
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="3"></td>
+                <td>Tiempo de Retorno Promedio:</td>
+                <td>
+                  {executionLog.length > 0 &&
+                    executionLog.reduce(
+                      (acc, curr) => acc + curr.turnaroundTime,
+                      0
+                    ) / executionLog.length}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>gantt</div>
+      </div>
     </div>
   );
 }
